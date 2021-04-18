94228138aef4ff75d8f2a9ad69b826060bab062d
Don Hatch <hatch@plunk.org>
2021-04-18 01:07:56 -0700

    experimental: print where it got stuck.
    E.g. on projecteuler #42:
      python2 sudoku.py 38..........4..785..9.2.3...6..9....8..3.2..9....4..7...1.7.5..495..6..........92
    Output includes:
      got stuck at:
       3     8     47    | 1567  156   157   | 9     2     146
       12    12    6     | 4     3     9     | 7     8     5
       57    457   9     | 15678 2     1578  | 3     146   146
      -------------------+-------------------+-------------------
       157   6     23    | 1578  9     1578  | 28    145   134
       8     1457  47    | 3     156   2     | 46    1456  9
       9     15    23    | 1568  4     158   | 28    7     136
      -------------------+-------------------+-------------------
       26    23    1     | 9     7     34    | 5     46    8
       4     9     5     | 2     8     6     | 1     3     7
       67    37    8     | 15    15    34    | 46    9     2
    
    This shows it misses cases of pointing and claiming, namely:
      - the 7 at r4c1 (pointed at by r4c46, or claimed by r5c23)
      - the two 5's at r3c46 (pointed at by r3c12, or claimed by r1c456)
      - the two 4's at r5c78 (pointed at by r5c23, or claimed by r4c89)

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,8 @@
 # A self-contained sudoku solver. Find me here: https://github.com/r1cc4rdo/sudoku
 
 from itertools import *
+import re
+import sys
 
 sudoku = """
 
@@ -17,30 +19,61 @@ sudoku = """
  . 9 . | . . . | 4 . . 
 
 """
-groups = tuple([] for _ in range(27))
-for index, (row, col) in enumerate(product(range(9), repeat=2)):
-    for displacement, rcs in enumerate((row, col, 3 * (row / 3) + col / 3)):
-        groups[rcs + displacement * 9].append(index)
+groups = ([set(range(row * 9, row * 9 + 9)) for row in range(9)] +
+          [set(range(col, 81, 9)) for col in range(9)] +
+          [set(((block/3*3 + i/3)*3 + block%3)*3 + i%3 for i in range(9)) for block in range(9)])
+rows_and_columns = groups[0:18]
 
+
+def dumpboard(board):
+    maxwidth = max(map(len, board))
+    print "maxwidth = %r"%maxwidth
+    line, div = '|'.join([' %-9s %-9s %-9s ']*3)+'\n', '+'.join(['-'*(maxwidth*3+4)]*3)+"\n"
+    line = re.sub(r'9', str(maxwidth), line)
+    print (line * 3 + div + line * 3 + div + line * 3) % tuple(board)
 
 def eliminate_candidates(board, groups=groups):
     for subset_size, group in product(range(1, 9), groups):
         for subset in combinations(group, subset_size):
             candidates_in_subset = set(''.join(board[index] for index in subset))
             if len(candidates_in_subset) == len(subset):  # we found a constraint
-                all_supersets = [g for g in groups if set(subset) <= set(g)]
-                for index in [index for g in all_supersets for index in g if index not in subset]:
+                original_board = board
+                subset_set = set(subset)
+                all_supersets = [g for g in groups if subset_set <= g]
+                for index in set(index for g in all_supersets for index in g if index not in subset_set):
                     board[index] = ''.join(c for c in board[index] if c not in candidates_in_subset)
                     assert board[index]  # if triggered, inconsistent assignment detected
 
 
+def transpose_digits_with_cols(board):
+    # for each row and digit, answer[row * 9 + digit] will be a string
+    # representing the col positions of digit in the row
+    return [''.join('123456789'[col] for col in range(9) if digit in board[row * 9 + col])
+            for row in range(9) for digit in '123456789']
+
+
+def eliminate_rowcolumn_fish(board):
+    board[:] = transpose_digits_with_cols(board)
+    eliminate_candidates(board, rows_and_columns)
+    board[:] = transpose_digits_with_cols(board)
+
+
+num_solves = 0
 def solve(board):
+    print "in solve:";
+    dumpboard(board)
+
+    global num_solves; num_solves += 1
     candidates, prev = 1 + 9**3, 2 + 9**3
     while 81 < candidates < prev:  # keep eliminating
-        prev, candidates = candidates, sum(map(len, board))
         eliminate_candidates(board)
+        #eliminate_rowcolumn_fish(board)
+        prev, candidates = candidates, sum(map(len, board))
     if candidates == 81:  # we're done!
         return board
+    print "got stuck at:"
+    dumpboard(board)
+
     lengths = map(len, board)  # otherwise, we need to search
     pivot = lengths.index(sorted(set(lengths))[1])
     for board[pivot] in board[pivot]:
@@ -52,6 +85,8 @@ def solve(board):
 
 
 if __name__ == '__main__':
-    board = list(islice([c if c.isdigit() else '123456789' for c in sudoku if c.isdigit() or c in '.0'], 81))
+    if len(sys.argv) == 2: sudoku = sys.argv[1]
+    board = list(islice((c if c in '123456789' else '123456789' for c in sudoku if c in '123456789.0'), 81))
     line, div = ' {} {} {} | {} {} {} | {} {} {} \n', '-------+-------+-------\n'
     print (line * 3 + div + line * 3 + div + line * 3).format(*solve(board))
+    print "num_solves = %r" % (num_solves,)
